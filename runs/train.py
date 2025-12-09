8ac0deb7187dd8409a9dd6c7cd0986363383e2c5
l-tiezerin <lucastiezerin@gmail.com>
2025-12-08 21:37:55 -0300

    first successfull runs in the experiment

--- runs/train.py
+++ runs/train.py
@@ -9,16 +9,40 @@ mlflow.is_tracking_uri_set()
 
 import pandas as pd
 from sklearn.datasets import load_breast_cancer
-from sklearn.model_selection import StratifiedKFold, RandomizedSearchCV, cross_validate
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
 # %%
 
-with mlflow.start_run(1):
+with mlflow.start_run(experiment_id=1):
     mlflow.sklearn.autolog()
 
     X, y = load_breast_cancer(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, train_size=.2, random_state=42
+    )
 
-    cv = 
+    params = {
+        'criterion': 'gini',
+        'max_depth': None,
+        'min_samples_split': 2,
+        'class_weight': None,
+    }
+    model = DecisionTreeClassifier(**params)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    metrics = {
+        'test_accuracy': accuracy_score(y_test, y_pred),
+        'test_precision': precision_score(y_test, y_pred),
+        'test_recall': recall_score(y_test, y_pred)
+    }
+
+    mlflow.log_metrics(metrics)
+
+# %%
+
 
